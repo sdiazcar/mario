7ed5de1c9f7f262f14b8f57a22690436a2b540f2
Stephany Diaz Cartagena <asocoba96@hotmail.com>
2018-10-27 07:05:09 -0500

    1

---
+++ UnityScriptingBasics/Assets/Scripts/GhostMotion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMotion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("One Shot");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(Time.time);
+
+        this.transform.position = new Vector3(Time.time, Mathf.Sin (Time.time) * 5, 0);
+    }
+}
